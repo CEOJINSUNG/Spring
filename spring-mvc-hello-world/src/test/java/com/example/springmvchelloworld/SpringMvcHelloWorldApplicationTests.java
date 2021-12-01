87023c40a6c8dc0b8f30ab5bbadec80b421e6c19
CEOJINSUNG <jinsung1048@gmail.com>
2021-12-01 19:57:32 +0900

    push

---
+++ spring-mvc-hello-world/src/test/java/com/example/springmvchelloworld/SpringMvcHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmvchelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
