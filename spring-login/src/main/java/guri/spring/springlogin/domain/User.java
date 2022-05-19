9d596c5382d39c37436f6c80434d9bcba4e3b066
CEOJINSUNG <jinsung1048@gmail.com>
2022-05-19 17:13:57 +0900

    jwt 연습하기

---
+++ spring-login/src/main/java/guri/spring/springlogin/domain/User.java
@@ -0,0 +1,44 @@
+package guri.spring.springlogin.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+
+    @JsonIgnore
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", length = 50, unique = true)
+    private String username;
+
+    @JsonIgnore
+    @Column(name = "password", length = 100)
+    private String password;
+    private String nickname;
+
+    @JsonIgnore
+    private boolean activated;
+
+    @ManyToMany
+    @JoinTable(
+        name = "user_authority",
+        joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "user_id")},
+        inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")}
+    )
+    private Set<Authority> authorities;
+}
